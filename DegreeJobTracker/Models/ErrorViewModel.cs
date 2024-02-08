d66d709f2d3994577104ceaee86145657e9f64c0
Nebarnard <nebarnard@senators.ws.edu>
2024-02-08 13:32:57 -0500

    Created Visual Studio Project and DatabaseSetup.sql.

---
+++ DegreeJobTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace DegreeJobTracker.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
