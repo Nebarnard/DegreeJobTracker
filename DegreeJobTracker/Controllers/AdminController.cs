d8ce94820bf3ff0f7826de3bc4e910543dc032c2
Nebarnard <nebarnard@senators.ws.edu>
2024-02-22 13:45:46 -0500

    Added Add/Edit Person Front-End.

--- DegreeJobTracker/Controllers/AdminController.cs
+++ DegreeJobTracker/Controllers/AdminController.cs
@@ -2,8 +2,19 @@
 
 namespace DegreeJobTracker.Controllers {
     public class AdminController : Controller {
+        // Admin Home
         public IActionResult Index() {
             return View();
-        }
-    }
-}
+        } // end method
+
+        // Add Person View
+        [HttpGet]
+        public IActionResult Person() {
+            // Action Name for page
+            ViewBag.Action = "Add";
+
+            return View(); 
+        } // end method
+
+    } // end class
+} // end namespace
