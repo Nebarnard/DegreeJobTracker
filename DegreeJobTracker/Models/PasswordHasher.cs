19d864c1da1505ca57aa1cbd301dfb2d5fbb31fb
Neb Barnard <nebarnard@senators.ws.edu>
2024-04-15 16:32:08 -0400

    Added Class for password hasing.

---
+++ DegreeJobTracker/Models/PasswordHasher.cs
@@ -0,0 +1,25 @@
+﻿namespace DegreeJobTracker.Models {
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class PasswordHasher {
+        public static string HashPassword(string password) {
+            using (SHA256 sha256 = SHA256.Create()) {
+                // Convert the password string to a byte array
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+                // Compute the hash
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++) {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
