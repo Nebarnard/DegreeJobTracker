cb21c391bad6067afc80fb0f3b34445b09e60929
Nebarnard <nebarnard@senators.ws.edu>
2024-04-19 03:19:27 -0400

    Created controller for account management views.

---
+++ DegreeJobTracker/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DegreeJobTracker.Controllers {
+    public class AccountController : Controller {
+        public IActionResult Index() {
+            return View();
+        } // end method
+
+
+        // Change Username
+        [HttpGet]
+        public IActionResult Username() {
+            return View();
+        } // end method
+
+        [HttpPost]
+        public IActionResult Username(string username) {
+            return RedirectToAction("Index");
+        } // end method
+
+
+        // Change Password
+        [HttpGet]
+        public IActionResult Password() {
+            return View();
+        } // end method
+
+        [HttpPost]
+        public IActionResult Password(string password) {
+            return RedirectToAction("Index");
+        } // end method
+
+
+        // Delete Credentials
+        [HttpGet]
+        public IActionResult Delete() {
+            return View();
+        } // end method
+
+        [HttpPost]
+        public IActionResult Delete(string delete) {
+            return RedirectToAction("Index");
+        } // end method
+    } // end class
+} // end namespace
