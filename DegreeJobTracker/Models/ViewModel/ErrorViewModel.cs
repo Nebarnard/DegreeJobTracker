e1204c643c94d7234c9aad2fcbed08066e6acf9f
Nebarnard <nebarnard@senators.ws.edu>
2024-04-22 05:17:19 -0400

    Set up Password Hasher for Github Pages.

--- DegreeJobTracker/Models/ViewModel/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace DegreeJobTracker.Models.ViewModel {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
